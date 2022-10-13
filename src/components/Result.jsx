045c7736c082cf2efda2cd21dcb79bc7a1779780
JordiTrassi <j.trassi@telefonica.net>
2022-10-13 07:53:45 +0200

    Added Result component styles

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,16 +1,50 @@
 import styled from '@emotion/styled';
 
+const Container = styled.div`
+    color: #FFF;
+    font-family: 'Lato', sans-serif;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 30px;
+`
+
+const Texto = styled.p`
+    font-size: 18px;
+    span {
+        font-weight: 700;
+    }   
+`
+
+const Precio = styled.p`
+    font-size: 25px;
+    span {
+        font-weight: 700;
+    }
+`
+
+const Image = styled.img`
+    display: block;
+    width: 120px;
+`
+
 export const Result = ({ resultado }) => {
     
     const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } = resultado;
     
   return (
-      <div>
-          <p>El Precio es de: <span>{PRICE}</span></p>
-          <p>El Precio más alto del día: <span>{HIGHDAY}</span></p>
-          <p>El Precio más bajo del día: <span>{LOWDAY}</span></p>
-          <p>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></p>
-          <p>Última Actualización: <span>{LASTUPDATE}</span></p>
-    </div>
+      <Container>
+          <Image
+              src={`http://cryptocompare.com/${IMAGEURL}`}
+              alt="crypto image"
+          />
+          <div>
+            <Precio>El Precio es de: <span>{PRICE}</span></Precio>
+            <Texto>El Precio más alto del día: <span>{HIGHDAY}</span></Texto>
+            <Texto>El Precio más bajo del día: <span>{LOWDAY}</span></Texto>
+            <Texto>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR} %</span></Texto>
+            <Texto>Última Actualización: <span>{LASTUPDATE}</span></Texto>
+          </div>
+    </Container>
   )
 }
